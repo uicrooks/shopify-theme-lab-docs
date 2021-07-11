462329d7b49c7ae21c8da9797851577d4682207c
Sergej Samsonenko <contact@sergej.codes>
2021-07-11 11:54:47 +0200

    add search

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,5 +10,20 @@ module.exports = {
     navbar,
     repo: 'https://github.com/uicrooks/shopify-theme-lab',
     repoLabel: 'GitHub'
-  }
+  },
+
+  plugins: [
+    // docs: https://v2.vuepress.vuejs.org/reference/plugin/search.html
+    [
+      '@vuepress/plugin-search',
+      {
+        isSearchable: (page) => page.path !== '/',
+        locales: {
+          '/': {
+            placeholder: 'Search (Press s)'
+          }
+        }
+      }
+    ]
+  ]
 }
