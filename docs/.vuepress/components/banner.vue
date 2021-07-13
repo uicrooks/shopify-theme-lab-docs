5e08708af976c4fc8cc7d5f4eea1529ee0d4aea1
Sergej Samsonenko <contact@sergej.codes>
2021-07-13 23:17:55 +0200

    adjust banner

--- docs/.vuepress/components/banner.vue
+++ docs/.vuepress/components/banner.vue
@@ -2,6 +2,10 @@
   <article v-if="url && src">
     <a :href="url">
       <img :src="src" loading="lazy">
+
+      <div class="note">
+        Get it 🔥
+      </div>
     </a>
   </article>
 </template>
@@ -21,13 +25,14 @@ export default {
 article {
   height: 225px;
   overflow: hidden;
+  border: 1px solid var(--c-border);
   margin: 2rem 0 1rem 0;
 
   a {
     display: block;
+    position: relative;
     width: 100%;
     height: 100%;
-    transition: all .125s ease-in-out;
   }
 
   img {
@@ -36,10 +41,28 @@ article {
     height: 100%;
     object-fit: cover;
     object-position: center center;
+    transition: all .125s ease-in-out;
   }
 
-  &:hover > a {
-    transform: scale(1.025);
+  .note {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background: #000;
+    color: white;
+    padding: 5px 10px;
+    transform: translateY(100%);
+    transition: all .125s ease-in-out;
+  }
+
+  &:hover {
+    a > img {
+      transform: scale(1.025);
+    }
+
+    .note {
+      transform: translateY(0);
+    }
   }
 }
 </style>
